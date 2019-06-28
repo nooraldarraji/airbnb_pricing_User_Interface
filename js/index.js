22dc00503e6d0812ff825a9afa1066e32f8225db
noorosce <077068@yahoo.com>
2019-06-28 13:12:49 -0500

    WIP: Added event listeners to index.html and removed some commented code from home.less

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Your code goes here
+
+// Mouseover
+
+let nB = document.querySelector(".des p");
+nB.addEventListener('mouseover', function(event) {
+    event.target.style.color = '#5cbf2ae6';
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 300);
+});
+
+// Double Click
+
+const fB = document.querySelector(".logo-heading");
+fB.addEventListener('dblclick', function(event) {
+    event.target.style.color = 'red';
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 300);
+})
